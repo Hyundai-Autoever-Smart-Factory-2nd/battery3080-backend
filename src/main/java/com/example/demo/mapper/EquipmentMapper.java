7b6e27bb83ad329fa1ff8df4a0f98d4032d7e077
jiyun319 <dlwldbs0319@gamil.com>
2025-08-25 17:55:26 +0900

    feat : 장비 mapper

---
+++ src/main/java/com/example/demo/mapper/EquipmentMapper.java
@@ -0,0 +1,20 @@
+package com.example.demo.mapper;
+
+import com.example.demo.model.Equipment;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+@Mapper
+public interface EquipmentMapper {
+    List<Equipment> findAll();  // 전체 장비 조회
+
+    Equipment findById(@Param("equipmentId") Long equipmentId);// 특정 장비 조회
+
+    void insert(Equipment equipment);  // 신규 장비 등록
+
+    void update(Equipment equipment);  // 장비 수정
+
+    void delete(@Param("equipmentId") Long equipmentId); // 장비 삭제
+}
