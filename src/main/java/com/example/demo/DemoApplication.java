7a6260b059cfef2889e3d35928d50c7727d3ad2f
asho227 <asho227@naver.com>
2025-08-26 14:51:36 +0900

    feat: EnableScheduling 제거

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,14 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+// import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableScheduling
-public class DemoApplication {
-
+// @EnableScheduling`
+public class DemoApplication
+{
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
-
 }
