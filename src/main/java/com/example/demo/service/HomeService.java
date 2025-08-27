ddda699b04657a50732d6581b427ef5cc4d59a58
asho227 <asho227@naver.com>
2025-08-27 14:32:38 +0900

    feat: DB에 할당된 값을 활용하는 코드로 변경

--- src/main/java/com/example/demo/service/HomeService.java
+++ src/main/java/com/example/demo/service/HomeService.java
@@ -22,20 +22,33 @@ public class HomeService {
         long failed = random.nextInt(10000) + 1L;
         long total = random.nextInt(10000) + 1L;
 
-        Double factory1Avg = homeMapper.getFactory1AvgBattery();
-        Double factory2Avg = homeMapper.getFactory2AvgBattery();
-        Double factory3Avg = homeMapper.getFactory3AvgBattery();
-        Long batteryWarning = homeMapper.getBatteryWarningCount();
+        Double f1Speed = homeMapper.getFactory1AvgSpeed();
+        Double f2Speed = homeMapper.getFactory2AvgSpeed();
+        Double f3Speed = homeMapper.getFactory3AvgSpeed();
 
-        return new Home(
+        Double f1Weight = homeMapper.getFactory1AvgWeight();
+        Double f2Weight = homeMapper.getFactory2AvgWeight();
+        Double f3Weight = homeMapper.getFactory3AvgWeight();
+
+        Long fixCount = homeMapper.getModelFixCount();
+
+        Home home = new Home(
                 target,
                 succeed,
                 failed,
                 total,
-                batteryWarning != null ? batteryWarning : 0L,
-                factory1Avg != null ? Math.round(factory1Avg * 100.0) / 100.0 : 0,
-                factory2Avg != null ? Math.round(factory2Avg * 100.0) / 100.0 : 0,
-                factory3Avg != null ? Math.round(factory3Avg * 100.0) / 100.0 : 0
+                fixCount != null ? fixCount : 0L,
+                0, 0, 0 // 예전 배터리 평균 대신 기본값 넣음
         );
+
+        home.setFactory1_avg_speed(f1Speed != null ? Math.round(f1Speed * 100.0) / 100.0 : 0);
+        home.setFactory2_avg_speed(f2Speed != null ? Math.round(f2Speed * 100.0) / 100.0 : 0);
+        home.setFactory3_avg_speed(f3Speed != null ? Math.round(f3Speed * 100.0) / 100.0 : 0);
+
+        home.setFactory1_avg_weight(f1Weight != null ? Math.round(f1Weight * 100.0) / 100.0 : 0);
+        home.setFactory2_avg_weight(f2Weight != null ? Math.round(f2Weight * 100.0) / 100.0 : 0);
+        home.setFactory3_avg_weight(f3Weight != null ? Math.round(f3Weight * 100.0) / 100.0 : 0);
+
+        return home;
     }
 }
