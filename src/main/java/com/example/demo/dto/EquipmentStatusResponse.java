70284b1124919fa47885bcfa04d2bac18f6fc10d
jiyun319 <dlwldbs0319@gamil.com>
2025-08-28 15:35:35 +0900

    fix : 주석 제거 및 임의값에서 실제 값으로 연동

--- src/main/java/com/example/demo/dto/EquipmentStatusResponse.java
+++ src/main/java/com/example/demo/dto/EquipmentStatusResponse.java
@@ -2,63 +2,43 @@ package com.example.demo.dto;
 
 public class EquipmentStatusResponse {
     private Long equipmentId;
-    private String name;
+    private Long modelInfosId; // 추가
     private String modelNum;
-    private int temp;       // 임의 temp 값
-    private String status;  // GOOD / WARNING / DANGER
+    private double temp;
+    private double cRate ;
+    private String status;// GOOD / WARNING / DANGER
 
     private EquipmentStatusResponse() {}
 
-    public EquipmentStatusResponse(Long equipmentId, String name, String modelNum, int temp, String status) {
+    public EquipmentStatusResponse(Long equipmentId, Long modelInfosId, String modelNum, double temp,double cRate, String status) {
         this.equipmentId = equipmentId;
-        this.name = name;
+        this.modelInfosId = modelInfosId;
         this.modelNum = modelNum;
         this.temp = temp;
+        this.cRate  = cRate;
         this.status = status;
     }
 
-    // Getter/Setter
+    public double getCRate() { return cRate; }
+    public void setCRate(double cRate) { this.cRate = cRate; }
+
+    public double getTemp() { return temp; }
+    public void setTemp(double temp) { this.temp = temp; }
+
+    public Long getModelInfosId() {
+        return modelInfosId;
+    }
+
+    public void setModelInfosId(Long modelInfosId) {
+        this.modelInfosId = modelInfosId;
+    }
+
     public Long getEquipmentId() { return equipmentId; }
     public void setEquipmentId(Long equipmentId) { this.equipmentId = equipmentId; }
-
-    public String getName() { return name; }
-    public void setName(String name) { this.name = name; }
 
     public String getModelNum() { return modelNum; }
     public void setModelNum(String modelNum) { this.modelNum = modelNum; }
 
-    public int getTemp() { return temp; }
-    public void setTemp(int temp) { this.temp = temp; }
-
     public String getStatus() { return status; }
     public void setStatus(String status) { this.status = status; }
 }
-
-
-
-//package com.example.demo.dto;
-//
-//import com.example.demo.model.Equipment;
-//import java.util.List;
-//
-//public class EquipmentStatusResponse {
-//    private List<Equipment> goodList;    // 양호
-//    private List<Equipment> warningList; // 주의
-//    private List<Equipment> dangerList;  // 경고
-//
-//    public EquipmentStatusResponse(List<Equipment> goodList, List<Equipment> warningList, List<Equipment> dangerList) {
-//        this.goodList = goodList;
-//        this.warningList = warningList;
-//        this.dangerList = dangerList;
-//    }
-//
-//    // getter/setter
-//    public List<Equipment> getGoodList() { return goodList; }
-//    public void setGoodList(List<Equipment> goodList) { this.goodList = goodList; }
-//
-//    public List<Equipment> getWarningList() { return warningList; }
-//    public void setWarningList(List<Equipment> warningList) { this.warningList = warningList; }
-//
-//    public List<Equipment> getDangerList() { return dangerList; }
-//    public void setDangerList(List<Equipment> dangerList) { this.dangerList = dangerList; }
-//}
