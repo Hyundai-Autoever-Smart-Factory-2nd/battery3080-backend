b8e5f2857269e933657b10d1fe5d9a44237c473f
jiyun319 <dlwldbs0319@gamil.com>
2025-08-28 15:34:22 +0900

    fix : api 주소 프런트와 통일하도록 수정

--- src/main/java/com/example/demo/controller/EquipmentController.java
+++ src/main/java/com/example/demo/controller/EquipmentController.java
@@ -1,12 +1,9 @@
 package com.example.demo.controller;
 
 import com.example.demo.dto.EquipmentDetailResponse;
-import com.example.demo.dto.EquipmentStatusGroupedResponse;
-import com.example.demo.dto.EquipmentStatusResponse;
 import com.example.demo.service.EquipmentService;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import com.example.demo.dto.EquipmentStatusPageResponse;
 
 @RestController
 @RequestMapping("/api/equipments")
@@ -23,22 +20,25 @@ public class EquipmentController {
      * Request: GET /api/equipments/status?equipmentId=1&factoryId=1&page=0&size=6
      * Response: List<EquipmentStatusResponse>
      */
-    @GetMapping("/status")
-    public EquipmentStatusGroupedResponse getEquipmentStatus(
-            @RequestParam("equipmentId") Long equipmentId,
-            @RequestParam("factoryId") Long factoryId,
+
+    @GetMapping("/{equipmentId}/factories/{factoryId}/status/{status}")
+    public EquipmentStatusPageResponse getEquipmentStatusByType(
+            @PathVariable Long equipmentId,
+            @PathVariable Long factoryId,
+            @PathVariable String status,
             @RequestParam(defaultValue = "0") int page,
-            @RequestParam(defaultValue = "6") int size
-    ) {
-        return equipmentService.getEquipmentStatus(equipmentId, factoryId, page, size);
+            @RequestParam(defaultValue = "6") int size) {
+
+        return equipmentService.getEquipmentStatusByType(equipmentId, factoryId, page, size, status);
     }
 
     /**
      * 장비 상세 조회 API
-     * Request: GET /api/equipments/{modelInfosId}/detail
+     * Request: GET /api/equipments/{modelInfosId}
      * Response: EquipmentDetailResponse
      */
-    @GetMapping("/{modelInfosId}/detail")
+
+    @GetMapping("/{modelInfosId}")
     public EquipmentDetailResponse getEquipmentDetail(
             @PathVariable("modelInfosId") Long modelInfosId
     ) {
